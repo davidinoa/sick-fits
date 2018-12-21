f73732ecc09a21449e10bd9bb8589c119ac3825e
David Inoa <davidinoa@gmail.com>
2018-12-21 09:45:46 -0400

    write first mutation and query

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,6 +15,6 @@ server.start(
     },
   },
   deets => {
-    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
