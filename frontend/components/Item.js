7a01d6eb763989e0d7f2393c14a78792e625f23d
David Inoa <davidinoa@gmail.com>
2019-08-07 15:29:24 -0700

    add items page and components

---
+++ frontend/components/Item.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import Title from './styles/Title';
+import formatMoney from './../lib/formatMoney';
+
+const Item = props => {
+  const { item } = props;
+  const { image, title, id, price, description } = item;
+
+  return (
+    <ItemStyles>
+      {image && <img src={image} alt={title} />}
+      <Title>
+        <Link href={{ pathname: '/item', query: { id } }}>
+          <a>{title}</a>
+        </Link>
+      </Title>
+      <PriceTag>{formatMoney(price)}</PriceTag>
+      <p>{description}</p>
+      <div className="buttonList">
+        <Link href={{ pathname: '/update', query: { id } }}>
+          <a>Edit ✏️</a>
+        </Link>
+        <button>Add To Cart</button>
+        <button>Delete</button>
+      </div>
+    </ItemStyles>
+  );
+};
+
+Item.propTypes = {
+  item: PropTypes.object.isRequired,
+};
+
+export default Item;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,8 @@
-import Link from 'next/link';
+import Items from '../components/Items';
 
-const Home = (props) => (
+const Home = props => (
   <div>
-    <p>Home page</p>
+    <Items />
   </div>
 );
 
